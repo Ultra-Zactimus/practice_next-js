abb04b34d86dfffaadd6294a4d4e3d8f85b01f3b
Ultra Zacky <ultrazacky@gmail.com>
2022-06-03 15:59:27 -0700

    add Link to About page on Home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import styles from "../styles/Home.module.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Link href="/about">About</Link>
       <h1>Hello World</h1>
     </div>
   )
