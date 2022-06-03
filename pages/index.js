7042b9801fd6e93e680d99b74575713faf00d17c
Ultra Zacky <ultrazacky@gmail.com>
2022-06-03 16:02:21 -0700

    add Header to Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import styles from "../styles/Home.module.css";
 import Link from 'next/link';
+import Head from 'next/head';
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Practice Next.js</title>
+      </Head>
       <Link href="/about">About</Link>
       <h1>Hello World</h1>
     </div>
-  )
+  );
 }
