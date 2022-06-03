ff9cd2f45b25df4e66ea46fe168dc2bc06cd84da
Ultra Zacky <ultrazacky@gmail.com>
2022-06-03 16:17:06 -0700

    add change to file naming

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-const about = () => {
+const About = () => {
   return (
     <div>
       <h1>About</h1>
@@ -6,4 +6,4 @@ const about = () => {
     );
 }
 
-export default about;
+export default About;
